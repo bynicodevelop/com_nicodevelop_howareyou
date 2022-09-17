edfe8ab0134c0a51a76f89bc7ea51af9bb5b59b3
nico-develop <contact@nico-develop.fr>
2022-09-17 10:38:48 +0200

    Ajout du fichier de configuration de theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,43 @@ import 'package:com_nicodevelop_howareyou/services/bloc/settings_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:json_theme/json_theme.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
 
   // Initialize the settings box
   final Box settingsBox = await Hive.openBox('user_settings_box');
 
+  /// Récupère le fichier de configuration theme
+  final themeStr = await rootBundle.loadString('assets/theme_config.json');
+
+  /// Conversion du fichier en objet
+  final themeJson = jsonDecode(themeStr);
+
+  /// Conversion en theme Flutter
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
   runApp(
     App(
       settingsBox: settingsBox,
+      theme: theme,
     ),
   );
 }
 
 class App extends StatelessWidget {
   final Box settingsBox;
+  final ThemeData theme;
 
   const App({
     super.key,
     required this.settingsBox,
+    required this.theme,
   });
 
   @override
@@ -40,10 +57,11 @@ class App extends StatelessWidget {
           ),
         ),
       ],
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        home: StartWizardScreen(),
+        theme: theme,
+        home: const StartWizardScreen(),
       ),
     );
   }
