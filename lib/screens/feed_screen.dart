38124532aa221a54a8970379a5be2297204ec862
nico-develop <contact@nico-develop.fr>
2022-09-18 11:54:18 +0200

    Ajout du prénom dans le point de démarrage - Fixed #13

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,7 +1,9 @@
 import 'package:com_nicodevelop_howareyou/config/moods_contants.dart';
 import 'package:com_nicodevelop_howareyou/models/mood_model.dart';
+import 'package:com_nicodevelop_howareyou/models/user_model.dart';
 import 'package:com_nicodevelop_howareyou/screens/how_are_you_screen.dart';
 import 'package:com_nicodevelop_howareyou/services/mood_list/mood_list_bloc.dart';
+import 'package:com_nicodevelop_howareyou/services/settings/settings_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -43,158 +45,166 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<MoodListBloc, MoodListState>(
-        bloc: context.read<MoodListBloc>()..add(OnListMoodEvent()),
-        builder: (context, state) {
-          List<MoodModel> moods = (state as MoodListInitialState).moods;
+      body: BlocBuilder<SettingsBloc, SettingsState>(
+        bloc: context.read<SettingsBloc>()..add(OnGetUserSettingsEvent()),
+        builder: (context, settingState) {
+          return BlocBuilder<MoodListBloc, MoodListState>(
+            bloc: context.read<MoodListBloc>()..add(OnListMoodEvent()),
+            builder: (context, state) {
+              final UserModel userModel =
+                  (settingState as SettingsLoadedState).userModel;
+              List<MoodModel> moods = (state as MoodListInitialState).moods;
 
-          return ListView.builder(
-            itemCount: moods.length,
-            padding: const EdgeInsets.only(
-              top: 100,
-              left: 32,
-              right: 32,
-              bottom: 16.0,
-            ),
-            itemBuilder: (context, index) {
-              if (index == moods.length - 1) {
-                return Column(
-                  children: [
-                    Stack(
+              return ListView.builder(
+                itemCount: moods.length,
+                padding: const EdgeInsets.only(
+                  top: 100,
+                  left: 32,
+                  right: 32,
+                  bottom: 16.0,
+                ),
+                itemBuilder: (context, index) {
+                  if (index == moods.length - 1) {
+                    return Column(
                       children: [
-                        Positioned(
-                          left: 20.5,
-                          top: 0,
-                          bottom: 0,
-                          child: Container(
-                            width: 6.0,
-                            color: Colors.grey[300],
-                          ),
+                        Stack(
+                          children: [
+                            Positioned(
+                              left: 20.5,
+                              top: 0,
+                              bottom: 0,
+                              child: Container(
+                                width: 6.0,
+                                color: Colors.grey[300],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 72.0,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 23.0,
+                                    backgroundColor: Theme.of(context)
+                                        .scaffoldBackgroundColor,
+                                    child: Text(
+                                      getFlatMoodsById(
+                                          moods[index].mood)["icon"],
+                                      style: const TextStyle(
+                                        fontSize: 36,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 16,
+                                  ),
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 6.0,
+                                      ),
+                                      child: Text(
+                                        moods[index].description,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            bottom: 72.0,
-                          ),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              CircleAvatar(
-                                radius: 23.0,
-                                backgroundColor:
-                                    Theme.of(context).scaffoldBackgroundColor,
+                        Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 72.0,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 23.0,
+                                    backgroundColor: Theme.of(context)
+                                        .scaffoldBackgroundColor,
+                                    child: const Text(
+                                      "👋",
+                                      style: TextStyle(
+                                        fontSize: 36,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 16,
+                                  ),
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 6.0,
+                                      ),
+                                      child: Text(
+                                        "Bienvenue ${userModel.firstname}",
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    );
+                  }
+
+                  return Stack(
+                    children: [
+                      Positioned(
+                        left: 20.5,
+                        top: 0,
+                        bottom: 0,
+                        child: Container(
+                          width: 6.0,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 72.0,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              radius: 23.0,
+                              backgroundColor:
+                                  Theme.of(context).scaffoldBackgroundColor,
+                              child: Text(
+                                getFlatMoodsById(moods[index].mood)["icon"],
+                                style: const TextStyle(
+                                  fontSize: 36,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Flexible(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 6.0,
+                                ),
                                 child: Text(
-                                  getFlatMoodsById(moods[index].mood)["icon"],
-                                  style: const TextStyle(
-                                    fontSize: 36,
-                                  ),
+                                  moods[index].description,
                                 ),
                               ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              Flexible(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                    top: 6.0,
-                                  ),
-                                  child: Text(
-                                    moods[index].description,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            bottom: 72.0,
-                          ),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                radius: 23.0,
-                                backgroundColor:
-                                    Theme.of(context).scaffoldBackgroundColor,
-                                child: const Text(
-                                  "👋",
-                                  style: TextStyle(
-                                    fontSize: 36,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              const Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                    top: 6.0,
-                                  ),
-                                  child: Text(
-                                    "Bienvenue",
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                );
-              }
-
-              return Stack(
-                children: [
-                  Positioned(
-                    left: 20.5,
-                    top: 0,
-                    bottom: 0,
-                    child: Container(
-                      width: 6.0,
-                      color: Colors.grey[300],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      bottom: 72.0,
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 23.0,
-                          backgroundColor:
-                              Theme.of(context).scaffoldBackgroundColor,
-                          child: Text(
-                            getFlatMoodsById(moods[index].mood)["icon"],
-                            style: const TextStyle(
-                              fontSize: 36,
                             ),
-                          ),
+                          ],
                         ),
-                        const SizedBox(
-                          width: 16,
-                        ),
-                        Flexible(
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              top: 6.0,
-                            ),
-                            child: Text(
-                              moods[index].description,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                    ],
+                  );
+                },
               );
             },
           );
